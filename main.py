dd08af9c71205135270e2c95251682d24a6169a4
John Doe <ademenet@student.42.fr>
2017-03-21 18:52:04 +0100

    Add todo list project

---
+++ main.py
@@ -0,0 +1,15 @@
+import cython
+import numpy as np
+import heapq
+
+# TODO generer le goal/arrivee du A*
+# TODO implementer 3 heuristiques qui ont du sens
+# TODO implementer le A*
+# TODO implementer la gestion de heap ? Priority Queue ? Heapq ?
+# TODO tester les performances a chaque fois
+
+def main():
+
+
+if __name__ == '__main__':
+	main()
