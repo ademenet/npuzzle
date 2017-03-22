64a2fb515481d3dc29f05ff5b72f7733d474f885
John Doe <ademenet@student.42.fr>
2017-03-22 19:22:17 +0100

    Working on A star algorithm, work in progress

---
+++ a_star.py
@@ -0,0 +1,36 @@
+import heapq
+import cython
+import numpy as np
+from heuristic import manhattan
+from debug import show_tree
+
+# WIP
+
+def retracePath(c):
+    path = [c]
+    while c.parent is not None:
+        c = c.parent
+        path.append(c)
+    path.reverse()
+    return path
+
+def solve(graph, current, end):
+	open_set = set()
+	close_set = set()
+	open_list = []
+	open_set.add(current)
+	open_list.append((0, current))
+	while open_set:
+		current = heapq.heappop(open_list)[1]
+		if current == end:
+			return retracePath(current)
+		open_set.remove(current)
+		close_set.add(current)
+		for tile in graph[current]:
+			if tile not in close_set:
+				tile.H = manhattan(end.x, end.y, tile.x, tile.y)
+				if tile not in open_set:
+					open_set.add(tile)
+					heapq.heappush(open_list, (tile.H, tile))
+				tile.parent = current
+	return open_list
