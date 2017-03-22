a5f89e7f325446e58c5683100c8a03093efdb608
John Doe <ademenet@student.42.fr>
2017-03-22 14:38:06 +0100

    Add main function

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import cython
 import numpy as np
 import heapq
+from goal_generator import goal_generator
 
-# TODO generer le goal/arrivee du A*
 # TODO implementer 3 heuristiques qui ont du sens
 # TODO implementer le A*
 # TODO implementer la gestion de heap ? Priority Queue ? Heapq ?
 # TODO tester les performances a chaque fois
 
-def main():
+import time
 
+def main():
+	start = time.time()
+	result = goal_generator(5)
+	end = time.time()
+	print(result)
+	print("total time exec: {}".format(end - start))
 
 if __name__ == '__main__':
 	main()
