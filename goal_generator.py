1e3e37a67ab6d030cf0223db8ad96edf34dd53cc
John Doe <ademenet@student.42.fr>
2017-03-22 11:23:01 +0100

    Add index and start number functions for goal spiral generator

--- goal_generator.py
+++ goal_generator.py
@@ -1,8 +1,28 @@
 def _get_index(size, i, j):
-	"""Get the index inside the spiral."""
+	"""Get the index inside the spiral.
+
+	In other words, we compute in which square belong the point of coordinates
+	(i, j). It's 0 for the outer square, 1 one inner, 2 and therefore.
+	For example, a 4*4 square:
+		0 0 0 0
+		0 1 1 0
+		0 1 1 0
+		0 0 0 0
+	Args:
+		size (int): puzzle size.
+		i (int): coordinate.
+		j (int): coordinate.
+	Returns:
+		square number (int) of the point belongs to.
+	"""
 	x = min(i, size - 1 - i)
 	y = min(j, size - 1 - j)
 	return min(x, y)
+
+def _start_number(size, k):
+	"""This functions returns the top left number (or the start number) of the
+	square k."""
+	return 4 * k * (size - k) + 1
 
 def goal_generator(size, dim=1):
 	"""This class generates the N-puzzle goal for the A star algorithm.
@@ -17,3 +37,4 @@ def goal_generator(size, dim=1):
 	if size < 3:
 		raise Exception("Can't generate goal for puzzle lower than 2.")
 	assert(dim == 1 or dim == 2), "Dimension should be 1 or 2."
+	
