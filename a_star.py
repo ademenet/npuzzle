8aef1e135dd251b88734137d50ec8b00897ceccb
John Doe <ademenet@student.42.fr>
2017-03-28 17:54:09 +0200

    Integrating A star solving, still uggy

--- a_star.py
+++ a_star.py
@@ -30,7 +30,7 @@ def from_2d_to_1d(size, i, j):
 	square."""
 	return i * size + j
 
-def neighbors(current, size):
+def neighbors(size, current):
 	"""It returns new states from the current state given in argument using a
 	yield generator."""
 	neighbors = [[-1, 0], [1, 0], [0, -1], [0, 1]]
@@ -43,39 +43,44 @@ def neighbors(current, size):
 			copy[index_zero] = current[to_switch_with]
 			copy[to_switch_with] = 0
 			yield copy
-"""
-def solve(start, end, size):
-	open_set = () # tuple des 'routes' a explorer
-	close_set = () # tuple des 'routes' explores et plus valable
+
+def manhattan(current, goal):
+	""""""
+	return np.sum(np.abs(np.subtract(current, goal)))
+
+def solve(start, goal, size):
+	open_set = [] # tuple des 'routes' a explorer
+	close_set = [] # tuple des 'routes' explores et plus valable
 	# open_list = [] # liste des etats en cours # test sans
 	heapq.heappush(open_set, (0, start))
 	# heapq.heappush(open_list, (0, start)) # test sans
 	# open_set.append((0, start)) # On ajoute le current dans la liste
 	# open_list.append((0, start)) # on initialise ici aussi
 	while open_set:
-		current = heapq.heappop(open_list)
-		if current[1] == end:
-			return retracePath(current)
-		open_set.remove(current) # on retire de la liste a explorer et on met dans la liste exploree
-		close_set.append(current)
-		for index in neighbors[current]: # on parcourt les possibilites
-			if tile not in close_set: # si on est pas deja passe par la hop !
-				# tile.H = manhattan(end.x, end.y, tile.x, tile.y)
-				heuristic = abs(x_1 - x_2) + abs(y_1 - y_2) # calcul de l'heuristic
-				if tile not in open_set: # si jamais explore, ajouter a la liste a explorer
-					open_set.add(tile)
-					heapq.heappush(open_list, (tile.H, tile)) # on fout tout avec lheuristic en classement
-				tile.parent = current # plus on sauvegarde le parent
+		current = heapq.heappop(open_set)
+		heapq.heappush(close_set, current)
+		print("current[1] = {}".format(current[1]))
+		if current[1] == goal:
+			print("END!")
+			# return retracePath(current)
+		# open_set.remove(current) # on retire de la liste a explorer et on met dans la liste exploree
+		# close_set.append(current)
+		for state in neighbors(size, current[1]): # on parcourt les possibilites
+			heuristic = manhattan(current[1], goal)
+			print("heuristic = {}".format(heuristic))
+
+			if state not in close_set: # si on est pas deja passe par la hop !
+				# tile.H = manhattan(goal.x, goal.y, tile.x, tile.y)
+				if state not in open_set: # si jamais explore, ajouter a la liste a explorer
+					# open_set.append((heuristic, state))
+					print(type(state))
+					heapq.heappush(open_set, (heuristic, state.tolist())) # on fout tout avec lheuristic en classement
+				# tile.parent = current # plus on sauvegarde le parent
 	return open_list
-"""
+
 # start = [1, 3, 2]
 # end = [1, 2, 3]
 # solve(neighbors, current, end)
-current = [4, 3, 5, 0, 1, 6, 2, 7, 8]
-print(np.reshape(current, (3, 3)))
-size = 3
-for index in neighbors(current, size):
-	# print (current)
-	print (index)
-	print (np.reshape(index, (3,3)))
-# neighbors(current, size)
+start = [4, 3, 5, 0, 1, 6, 2, 7, 8]
+goal = [1, 2, 3, 8, 0, 4, 7, 6, 5]
+solve(start, goal, 3)
