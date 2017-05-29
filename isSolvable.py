82bd0743a3e0494b826177c6ee8629f181e999df
Amine DERRAGUI <aderragu@e2r9p16.42.fr>
2017-05-29 15:12:12 +0200

    modif format comments

--- isSolvable.py
+++ isSolvable.py
@@ -1,11 +1,11 @@
-# Check for each value wether the right hand values are superior.
-#  If true, increment the 'inversion' variable for each superior right hand value.
-
 def isSolvable(puzzle):
+	"""Check for each value wether the right hand values are superior.
+	If true, increment the 'inversion' variable for each superior right hand value found.
+	"""
 	inversion = 0
 	for current in range(len(puzzle) - 1):
 		for i in range(current + 1, len(puzzle)):
 			if puzzle[current] > puzzle[i]:
 				inversion += 1
-	# Is solvable is the number of inversion is even
+	# It's solvable if the number of inversion is even
 	return not inversion % 2
