d7c9837e4e15a432ee923b48fb2f770090a96018
Amine DERRAGUI <aderragu@e2r9p14.42.fr>
2017-05-30 18:17:18 +0200

    euclidian distance added

--- heuristic.py
+++ heuristic.py
@@ -23,7 +23,7 @@ def distance_2_points(coord_ref, coord):
 	return abs(coord_ref[0] - coord[0]) + abs(coord_ref[1] - coord[1])
 
 def manhattan(state, goal,s):
-	"""This is the Manhatthan implementation.
+	"""This is the Manhatthan heuristic.
 
 		SUM(for i from 1 to s-1) : abs(Xgoal - Xstat_i) + abs(Ygoal - Ystat_i)
 
@@ -37,8 +37,6 @@ def manhattan(state, goal,s):
 	"""
 	heur = 0
 	i = 0
-	coord_ref = []
-	coord = []
 	s=s*s
 	for val in range(1, s):
 		coord_ref = find_coord(goal, val, s)
@@ -73,12 +71,44 @@ def nSwap(state, goal, s):
 	return heur
 
 def out_row_column(state, goal, s):
+	""""out of row out of column heuristic
+	
+	 	Number of tiles out of row + Number of tiles out of
+		column
 
+		Args:
+			state (int[]) : a puzzle state
+			goal (int[]) : the puzzle goal e.g final state 
+
+		Returns:
+			heuristic value (int).		
+	"""
 	heur = 0
 	for tile in range(1, s*s):
 		coord = from_1d_to_2d(s, state.index(tile))
 		coord_ref = from_1d_to_2d(s, goal.index(tile))
 		if coord[0] != coord_ref[0]:heur += 1
 		if coord[1] != coord_ref[1]:heur += 1
+	return heur
 
+def euclidian_distane(state, goal,s):
+	"""This is the euclidian distance heuristic.
+
+		SUM(for i from 1 to s*s-1) : sqrt ((Xgoal - Xstat_i)^2 + abs(Ygoal - Ystat_i)^2)
+
+		Args:
+			state (int[]) : a puzzle state
+			goal (int[]) : the puzzle goal e.g final state 
+			s (int) : size of the puzzle 
+
+		Returns:
+			heuristic value (int).   
+	"""
+	heur = 0
+	i = 0
+	n=s*s
+	for val in range(1, n):
+		coord_ref = from_1d_to_2d(s,goal.index(val))
+		coord = from_1d_to_2d(s,state.index(val))
+		heur += math.sqrt((coord_ref[0] - coord[0])**2 + (coord_ref[1] - coord[1])**2)
 	return heur
