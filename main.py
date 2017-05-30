fbcfd9d33c6491c50aa3106717eeabd957a01bdc
Alain DEMENET <ademenet@e2r8p15.42.fr>
2017-05-30 17:50:00 +0200

    First argument parsing and generating puzzle

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from goal_generator import goal_generator
 from a_star import solve
+import argparse
+from parsing import parse 
+from puzzle_generator import puzzle_generator
+import sys
 
 # TODO implementer le A*: allmost done
 
@@ -22,25 +26,49 @@ from a_star import solve
 
 import time
 
+def argparseErrorHeuristic(string):
+    print(string)
+    if string.casefold() != "manhattan" or string != "h2" or string != "h3":
+        raise argparse.ArgumentTypeError("This heuristic doesn't exist")
+    return string
+
 if __name__ == '__main__':
-	# start = [1, 3, 2]
-	# end = [1, 2, 3]
-	# solve(neighbors, current, end)
-	# start = [6, 5, 4, 1, 0, 8, 7, 2, 3]
-	# start = [3,5,0,1,4,2,7,6,8]
-	# start = [5,3,7,1,6,2,0,8,4]
-	start = [8,1,7,5,4,6,2,3,0]
-	# goal = goal_generator(3, 1)
-	# TODO: format goal
-	goal = [1, 2, 3, 8, 0, 4, 7, 6, 5]
-	print("start: {} \ngoal: {}".format(start, goal))
-	parent = solve(start, goal, 3)
+    parser = argparse.ArgumentParser(description='This software solve n-puzzle using A-star algorithm')
+    parser.add_argument('filename', nargs='?',
+                        type=str, default=None,
+                        help='text file describing the n-puzzle initial state')
+    parser.add_argument('-h1', '--manhattan', action='store_true', help='choose manhattan heuristic')
+    parser.add_argument('-h2', '--nswap', action='store_true', help='choose n-swap heuristic')
+    parser.add_argument('-h3', '--euclidean', action='store_true', help='choose euclidean heuristic')
+    parser.add_argument('-s', '--size', type=int, default=3, help='choose a particular size to random generated n-puzzle')
+    args = vars(parser.parse_args())
 
-	# def main():
-	# 	start = time.time()
-	# 	result = goal_generator(3, 1)
-	# 	end = time.time()
-	# 	print(result)
-	# 	print("total time exec: {}".format(end - start))
+    if args['filename'] is not None:
+        npuzzle = parse(args['filename']) 
+    else:
+        if args['size'] > 2:
+            npuzzle = puzzle_generator(args['size'] * args['size'])
+        else:
+            sys.exit("Size is too small")
+    print(npuzzle)
+    # start = [1, 3, 2]
+    # end = [1, 2, 3]
+    # solve(neighbors, current, end)
+    # start = [6, 5, 4, 1, 0, 8, 7, 2, 3]
+    # start = [3,5,0,1,4,2,7,6,8]
+    # start = [5,3,7,1,6,2,0,8,4]
+    # start = [8,1,7,5,4,6,2,3,0]
+    # goal = goal_generator(3, 1)
+    # TODO: format goal
+    # goal = [1, 2, 3, 8, 0, 4, 7, 6, 5]
+    # print("start: {} \ngoal: {}".format(start, goal))
+    # parent = solve(start, goal, 3)
 
-	# main()
+    # def main():
+    # 	start = time.time()
+    # 	result = goal_generator(3, 1)
+    # 	end = time.time()
+    # 	print(result)
+    # 	print("total time exec: {}".format(end - start))
+
+    # main()
