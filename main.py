d49a827cd99464fb7d1926b3b70d5a816e8f3933
Alain DEMENET <ademenet@e2r7p19.42.fr>
2017-06-01 18:10:19 +0200

    Ajout de timeit pour les perfs chronometrees

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
+import argparse
+import sys
+from timeit import default_timer as timer
 from goal_generator import goal_generator
 from a_star import solve
-import argparse
 from parsing import parse
 from puzzle_generator import puzzle_generator
-import sys
 from isSolvable import isSolvable
-
-# TODO implementer le A*: allmost done
-
-# TODO implementer 3 heuristiques qui ont du sens
-# 	TODO manhattan
-#	TODO one other
-#	TODO one other
 
 # TODO faire le retour du nombre de opened states
 # TODO faire le retour du nombre de states en memoire durant la recherche (opened + closed)
 # TODO faire le retour du nombre de moves qu'il a fallut
 # TODO faire le retour des differents etats qui menent a la solution
 
-# TODO faire un parser
-# TODO faire un detecteur de solvalbilite
-
-# TODO fournir differntes maps pour les tests de la correction
-
-# TODO bonuses
-
-import time
-
-if __name__ == '__main__':
+def _argparser():
+    """Parse arguments using argparse library, returns a dictionnary with values."""
     parser = argparse.ArgumentParser(description='This software solve n-puzzle using A-star algorithm')
     parser.add_argument('filename', nargs='?',
                         type=str, default=None,
@@ -36,11 +22,19 @@ if __name__ == '__main__':
     parser.add_argument('-h2', '--nswap', action='store_true', help='choose n-swap heuristic')
     parser.add_argument('-h3', '--euclidean', action='store_true', help='choose euclidean heuristic')
     parser.add_argument('-s', '--size', type=int, default=3, help='choose a particular size to random generated n-puzzle')
+    parser.add_argument('-st', '--stats', action='store_true', default=False, help='display the timer stats for the principal functions')
     args = vars(parser.parse_args())
+    return args
+
+def main():
+    args = _argparser()
 
     if args['filename'] is not None:
-        npuzzle = parse(args['filename'])
-        if not isSolvable(npuzzle):
+        start = timer()
+        npuzzle, size = parse(args['filename'])
+        end = timer()
+        if args['stats']: print("Parsing took: ", end - start, " s")
+        if not isSolvable(npuzzle, size):
             sys.exit("Puzzle is not solvable")
     else:
         if args['size'] > 2:
@@ -48,34 +42,19 @@ if __name__ == '__main__':
         else:
             sys.exit("Size is too small")
 
-    print("Start: ", npuzzle)
+    print("Initial state: ", npuzzle)
 
     print("--- Generating goal state")
     goal = goal_generator(args['size'], dim=1)
-    print("Goal: ", goal)
+    print("Goal state: ", goal)
 
     print("--- Solving puzzle using A-star")
+    start = timer()
     solve(npuzzle, goal, args['size'])
+    end = timer()
+    if args['stats']: print("Solving took: ", end - start, " s")
 
     print("--- END")
-    # start = [1, 3, 2]
-    # end = [1, 2, 3]
-    # solve(neighbors, current, end)
-    # start = [6, 5, 4, 1, 0, 8, 7, 2, 3]
-    # start = [3,5,0,1,4,2,7,6,8]
-    # start = [5,3,7,1,6,2,0,8,4]
-    # start = [8,1,7,5,4,6,2,3,0]
-    # goal = goal_generator(3, 1)
-    # TODO: format goal
-    # goal = [1, 2, 3, 8, 0, 4, 7, 6, 5]
-    # print("start: {} \ngoal: {}".format(start, goal))
-    # parent = solve(start, goal, 3)
 
-    # def main():
-    # 	start = time.time()
-    # 	result = goal_generator(3, 1)
-    # 	end = time.time()
-    # 	print(result)
-    # 	print("total time exec: {}".format(end - start))
-
-    # main()
+if __name__ == '__main__':
+    main()
