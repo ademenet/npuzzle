0e42d033358e512572ce855ac4ad51af95b2f2a4
moannuo <ademenet@e2r8p15.42.fr>
2017-06-09 12:54:39 +0200

    Some optimizations

--- isSolvable.pyx
+++ isSolvable.pyx
@@ -3,26 +3,30 @@ import numpy as np
 cimport numpy as np
 from utils import *
 
-def inversion(puzzle):
+def inversion(np.ndarray puzzle):
 	"""Check for each value wether the right hand values are inferior. If true,
 	increment the 'inversion' variable for each superior right hand value found.
 
 	Args:
 		puzzle (1D numpy array): the puzzle to check, in is initial state form.
 	"""
+	print(puzzle)
 	cdef int inversion = 0
 	cdef int current
 	cdef int i
-	for current in range(puzzle.size - 1):
-		for i in range(current + 1, puzzle.shape[0]):
-			if puzzle[current] > puzzle[i]:
+	cdef int N = puzzle.size - 1
+	cdef int D = puzzle.shape[0]
+	for current in range(N):
+		for i in range(current + 1, D):
+			if puzzle[current] > puzzle[i] and (puzzle[current] != 0 and puzzle[i] != 0):
 				inversion += 1
 	return inversion
 
 def isSolvable(puzzle, goal, int size):
-	"""Check if a uzzle is solvable.
+	"""Check if a puzzle is solvable.
 
-	compare the polarity of the numbers of inversion between the puzzle and the goal
+	Compare the polarity of the numbers of inversion between the puzzle and the
+	goal.
 
 	Args:
 		puzzle (1D numpy array): the puzzle to check, in is initial state form.
@@ -37,7 +41,6 @@ def isSolvable(puzzle, goal, int size):
 	if (puzzle.size % 2 == 0):
 		inversion_puzzle += (size - from_1d_to_2d(size, np.where(puzzle == 0)[0])[0]) % 2
 		inversion_goal += (size - from_1d_to_2d(size, np.where(goal == 0)[0])[0]) % 2
-		print (type(inversion_puzzle))
 	if (inversion_puzzle % 2) == (inversion_goal % 2):
 		return True
 	else:
